88e8865a13e3674fa0eb89cc249c367d04550de2
ChangHuii <Alek.chr@hotmail.com>
2018-06-01 03:21:24 +0200

    Fixed order storage

--- ContractExchange/Contract.cs
+++ ContractExchange/Contract.cs
@@ -1,8 +1,5 @@
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Services.Neo;
-using Neo.SmartContract.Framework.Services.System;
-using System;
-using System.ComponentModel;
 using System.Numerics;
 
 
@@ -10,12 +7,6 @@ namespace ExchangeContract
 {
     public class Contract : SmartContract
     {
-
-        [DisplayName("created")]
-        public static event Action<byte[], byte[], byte[], BigInteger, byte[], BigInteger> Created; 
-
-        [DisplayName("accepted")]
-        public static event Action<byte[], byte[], BigInteger, byte[], BigInteger, byte[], BigInteger> Accepted;
 
         private static readonly byte[] Empty = { };
         public delegate object NEP5Contract(string method, object[] args);
@@ -39,33 +30,41 @@ namespace ExchangeContract
         )
         {
             Order order = new Order();
-            
-                order.CreatorAddress = creatorAddress;
-                order.OfferTokenID = offerTokenID;
-                order.OfferAmount = offerAmount.AsBigInteger();
-                order.WantTokenID = wantAssetID;
-                order.WantAmount = wantAmount.AsBigInteger();
-                order.AvailableAmount = availableAmount.AsBigInteger();
-                order.Nonce = nonce;
+
+            order.CreatorAddress = creatorAddress;
+            order.OfferTokenID = offerTokenID;
+            order.OfferAmount = offerAmount.AsBigInteger();
+            order.WantTokenID = wantAssetID;
+            order.WantAmount = wantAmount.AsBigInteger();
+            order.AvailableAmount = availableAmount.AsBigInteger();
+            order.Nonce = nonce;
             return order;
         }
 
 
+        /*    Parameter types: 0710
+         *    Return type: 05
+         * 
+         **/
 
-
-        public static bool Main(string operation, params object[] args)
+        public static object Main(string operation, params object[] args)
         {
             switch (operation)
             {
                 case "deposit":
+                    if (args.Length != 3) return false;
                     return Deposit((byte[])args[0], (byte[])args[1], (BigInteger)args[2]);
                 case "create":
+                    if (args.Length != 7) return false;
                     return CreateOrder(
-                     NewOrder((byte[])args[0], (byte[])args[1], (byte[])args[2], (byte[])args[3], (byte[])args[4], (byte[])args[5], (byte[])args[6]));
+                    NewOrder((byte[])args[0], (byte[])args[1], (byte[])args[2], (byte[])args[3], (byte[])args[4], (byte[])args[5], (byte[])args[6]));
                 case "accept":
+                    if (args.Length != 4) return false;
                     return AcceptOrder((byte[])args[0], (byte[])args[1], (byte[])args[2], (BigInteger)args[3]);
                 case "remove":
+                    if (args.Length != 2) return false;
                     return RemoveOrder((byte[])args[0], (byte[])args[1]);
+                
 
             }
             return false;
@@ -73,8 +72,9 @@ namespace ExchangeContract
 
         private static bool Deposit(byte[] userAddress, byte[] depositAssetID, BigInteger depositAmount)
         {
-            if (!VerifySentAmount(userAddress, depositAssetID, depositAmount)) return false;
-            TransferToken(userAddress, depositAssetID, depositAmount);
+            byte[] key = BalanceKey(userAddress, depositAssetID);
+            BigInteger currentBalance = Storage.Get(Context(), key).AsBigInteger();
+            Storage.Put(Context(), key, currentBalance + depositAmount);
             return true;
         }
 
@@ -83,7 +83,6 @@ namespace ExchangeContract
             if (!Runtime.CheckWitness(order.CreatorAddress)) return false;
 
             // Update balance
-            if (!(order.OfferAmount > 0 && order.WantAmount > 0)) return false;
             if (!UpdateBalance(order.CreatorAddress, order.OfferTokenID, order.OfferAmount)) return false;
 
             // Add the order to storage
@@ -91,8 +90,6 @@ namespace ExchangeContract
             var orderHash = Hash(order);
             if (Storage.Get(Storage.CurrentContext, tradingPair.Concat(orderHash)) != Empty) return false;
             StoreOrder(tradingPair, orderHash, order);
-
-            Created(order.CreatorAddress, orderHash, order.OfferTokenID, order.OfferAmount, order.WantTokenID, order.WantAmount);
             return true;
         }
 
@@ -129,15 +126,13 @@ namespace ExchangeContract
             order.AvailableAmount = order.AvailableAmount - amountToTake;
 
             // Reduce available balance for the filled token and amount
-            if(!UpdateBalance(fillerAddress, order.WantTokenID, amountToFill)) return false;
+            if (!UpdateBalance(fillerAddress, order.WantTokenID, amountToFill)) return false;
 
             // Move token to the taker and creator
             TransferToken(fillerAddress, order.OfferTokenID, amountToTake);
             TransferToken(order.CreatorAddress, order.WantTokenID, amountToFill);
 
             StoreOrder(tradingPair, orderHash, order);
-
-            Accepted(fillerAddress, orderHash, amountToFill, order.OfferTokenID, order.OfferAmount, order.WantTokenID, order.WantAmount);
             return true;
         }
 
@@ -152,7 +147,7 @@ namespace ExchangeContract
             // Otherwise, store the order   
             else
             {
-                Runtime.Log("Serializing order");
+                Runtime.Log("Storing order");
                 var orderData = order.Serialize();
                 Storage.Put(Storage.CurrentContext, tradingPair.Concat(orderHash), orderData);
             }
@@ -187,55 +182,6 @@ namespace ExchangeContract
             return true;
         }
 
-        private static bool VerifySentAmount(byte[] originator, byte[] assetID, BigInteger amount)
-        {
-            // Verify that the offer really has the indicated assets available
-            if (assetID.Length == 32)
-            {
-                // Check the current transaction for the system assets
-                var currentTxn = (Transaction)ExecutionEngine.ScriptContainer;
-                var outputs = currentTxn.GetOutputs();
-                ulong sentAmount = 0;
-                foreach (var o in outputs)
-                {
-                    if (o.AssetId == assetID && o.ScriptHash == ExecutionEngine.ExecutingScriptHash)
-                    {
-                        sentAmount += (ulong)o.Value;
-                    }
-                }
-
-                // Check that the sent amount is correct
-                if (sentAmount != amount)
-                {
-                    return false;
-                }
-
-                // Check that there is no double deposit
-                var alreadyVerified = Storage.Get(Context(), currentTxn.Hash.Concat(assetID)).Length > 0;
-                if (alreadyVerified) return false;
-
-               
-                Storage.Put(Context(), currentTxn.Hash.Concat(assetID), 1);
-
-                return true;
-            }
-            else if (assetID.Length == 20)
-            {
-                if (!VerifyContract(assetID)) return false;
-                var args = new object[] { originator, ExecutionEngine.ExecutingScriptHash, amount };
-                var Contract = (NEP5Contract)assetID.ToDelegate();
-                var transferSuccessful = (bool)Contract("transfer", args);
-                return transferSuccessful;
-            }
-            return false;
-        }
-
-        private static bool VerifyContract(byte[] assetID)
-        {
-            if (Storage.Get(Context(), "stateContractWhitelist") == Inactive) return true;
-            return Storage.Get(Context(), WhitelistKey(assetID)).Length > 0;
-        }
-
         private static Order GetOrder(byte[] tradingPair, byte[] hash)
         {
             byte[] orderData = Storage.Get(Storage.CurrentContext, tradingPair.Concat(hash));
@@ -255,15 +201,9 @@ namespace ExchangeContract
             return Hash256(bytes);
         }
 
-
         private static StorageContext Context() => Storage.CurrentContext;
         private static byte[] TradingPair(Order o) => o.OfferTokenID.Concat(o.WantTokenID);
         private static byte[] BalanceKey(byte[] trader, byte[] assetID) => trader.Concat(assetID);
-        private static byte[] WhitelistKey(byte[] assetID) => "contractWhitelist".AsByteArray().Concat(assetID);
-
-
-
-
 
     }
 
